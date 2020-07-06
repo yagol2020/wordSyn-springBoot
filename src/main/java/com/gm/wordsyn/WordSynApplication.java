2f3b8ddf244e885eacd711b60e0e711241fc8f55
yagol <yhw_yagol@sina.com>
2020-07-06 21:50:59 +0800

    change

--- src/main/java/com/gm/wordsyn/WordSynApplication.java
+++ src/main/java/com/gm/wordsyn/WordSynApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WordSynApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WordSynApplication.class, args);
     }
